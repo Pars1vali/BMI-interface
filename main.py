4a1493a3c370c090d80537882eb694aa45ae17a9
Chumak Vladimir <parsival427@gmail.com>
2023-02-04 12:07:27 +0300

    twice commit add new widget - change theme also new design and style with font - Terminal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-
-
+isLight = True
 
 def calculate_bmi():
     kg = int(weight_tf.get())
@@ -9,54 +8,83 @@ def calculate_bmi():
     bmi = kg/(m*m)
     bmi = round(bmi, 1)
 
-
     if bmi < 18.5:
-        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует недостаточному весу')
+        messagebox.showinfo('', f'ИМТ = {bmi} соответствует недостаточному весу')
     elif (bmi >= 18.5) and (bmi <= 24.9):
-        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует нормальному весу')
+        messagebox.showinfo('', f'ИМТ = {bmi} соответствует нормальному весу')
     elif (bmi >= 24.9) and (bmi <= 29.9):\
-        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует избыточному весу')
+        messagebox.showinfo('', f'ИМТ = {bmi} соответствует избыточному весу')
     else:
-        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует ожирению')
+        messagebox.showinfo('', f'ИМТ = {bmi} соответствует ожирению')
+def change_color():
+    global isLight
+    isLight = not isLight
+
+    if isLight:
+        window.config(bg=baseColor)
+        height_lb.config(bg=baseColor)
+        height_lb.config(bg = baseColor)
+        height_lb.config(fg="black")
+        weight_lb.config(bg=baseColor)
+        weight_lb.config(fg="black")
+        btn_theme.config(text="Dark theme")
+        btn_theme.config(fg="white", bg="black")
+
+    else:
+        window.config(bg="black")
+        height_lb.config(bg = "black")
+        height_lb.config(fg="white")
+        weight_lb.config(bg="black")
+        weight_lb.config(fg="white")
+        btn_theme.config(text="Light theme")
+        btn_theme.config(fg="black", bg="white")
+def history():
+    return 1
 
 window = Tk()
 window.title("Калькулятор индекса массы тела (ИМТ)")
-window.geometry('400x300+600+200')
-
-
-frame = Frame(
-    window,
-    padx=10,
-    pady=10
-)
-frame.pack(expand=True)
+window.geometry('500x300')
+baseColor = window.cget("background")
+window.config(bg=baseColor)
+window.resizable(width=False, height=False)
 
 height_lb = Label(
-    frame,
-    text="Введите свой рост (в см)"
+    text="Введите свой рост (в см)",
+    fg="black"
 )
-height_lb.grid(row=3, column=1)
+height_lb.place(x=90, y=90)
 
-weight_lb = Label(
-    frame,
-    text="Введите свой вес (в кг) "
-)
-weight_lb.grid(row=4, column=1)
+weight_lb = Label(text="Введите свой вес (в кг) ")
+weight_lb.place(x=270, y=90)
 
-height_tf = Entry(
-    frame,
-)
-height_tf.grid(row = 3,column=2, pady=5)
+height_tf = Entry()
+height_tf.place(x=96.5, y=110, width=130)
 
-weight_tf = Entry(
- frame,
-)
-weight_tf.grid(row=4, column=2, pady=5)
+weight_tf = Entry()
+weight_tf.place(x=270, y=110, width=130)
 
 cal_btn = Button(
- frame,
- text='Рассчитать ИМТ',
- command=calculate_bmi
+ text='BMI',
+ command=calculate_bmi,
+ font=('Terminal',10)
 )
-cal_btn.grid(row=5, column=2)
+cal_btn.place(x=185, y=160)
+
+
+btn_theme = Button(
+    bg = "black",
+    fg="white",
+    text="Dark theme",
+    command = change_color,
+    font=('Terminal', 10)
+)
+btn_theme.place(x=200, y=250)
+
+btn_history = Button(
+    text="History",
+    font=("Terminal", 10),
+    command=history
+)
+btn_history.place(x=250, y=160)
+
 window.mainloop()
