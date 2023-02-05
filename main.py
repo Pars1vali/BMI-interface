6b53ec52620deb66161f7cae0c2fe32755f17f5d
Chumak Vladimir <parsival427@gmail.com>
2023-02-05 19:35:39 +0300

    3 commit add widget - history he work)

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from tkinter import messagebox, ttk
 
 isLight = True
 
+
 def calculate_bmi():
     kg = int(weight_tf.get())
-    m = int(height_tf.get())/100
-    bmi = kg/(m*m)
+    m = int(height_tf.get()) / 100
+    bmi = kg / (m * m)
     bmi = round(bmi, 1)
     with open("history.txt", 'a') as file:
         file.write(str(bmi) + " " + str(kg) + " " + str(m) + "\n")
@@ -15,10 +16,12 @@ def calculate_bmi():
         messagebox.showinfo('', f'ИМТ = {bmi} соответствует недостаточному весу')
     elif (bmi >= 18.5) and (bmi <= 24.9):
         messagebox.showinfo('', f'ИМТ = {bmi} соответствует нормальному весу')
-    elif (bmi >= 24.9) and (bmi <= 29.9):\
-        messagebox.showinfo('', f'ИМТ = {bmi} соответствует избыточному весу')
+    elif (bmi >= 24.9) and (bmi <= 29.9): \
+            messagebox.showinfo('', f'ИМТ = {bmi} соответствует избыточному весу')
     else:
         messagebox.showinfo('', f'ИМТ = {bmi} соответствует ожирению')
+
+
 def change_color():
     global isLight
     isLight = not isLight
@@ -26,7 +29,7 @@ def change_color():
     if isLight:
         window.config(bg=baseColor)
         height_lb.config(bg=baseColor)
-        height_lb.config(bg = baseColor)
+        height_lb.config(bg=baseColor)
         height_lb.config(fg="black")
         weight_lb.config(bg=baseColor)
         weight_lb.config(fg="black")
@@ -35,40 +38,47 @@ def change_color():
 
     else:
         window.config(bg="black")
-        height_lb.config(bg = "black")
+        height_lb.config(bg="black")
         height_lb.config(fg="white")
         weight_lb.config(bg="black")
         weight_lb.config(fg="white")
         btn_theme.config(text="Light theme")
         btn_theme.config(fg="black", bg="white")
+
+
 def history():
     history_w = Tk()
-    history_w.title("History")
-    history_w.geometry("250x200+500+350")
-    # history_w.config(bg=baseColor)
-    # history_w.resizable(0, 0)
+    history_w.title('History')
+    history_w.geometry('255x200+500+350')
+    history_w.config(bg=baseColor)
+    history_w.resizable(0, 0)
 
-    people = [
-        ("Tom", 38, "tom@email.com"), ("Bob", 42, "bob@email.com"), ("Sam", 28, "sam@email.com"),
-        ("Alice", 33, "alice@email.com"), ("Kate", 21, "kate@email.com"), ("Ann", 24, "ann@email.com"),
-        ("Mike", 34, "mike@email.com"), ("Alex", 52, "alex@email.com"), ("Jess", 28, "jess@email.com"),
-    ]
+    set = ttk.Treeview(history_w)
+    set.pack()
 
-    columns = ("name", "age", "email")
+    set['columns'] = ('BMI', 'Weight', 'Height')
+    set.column("#0", width=0, stretch=NO)
+    set.column("BMI", anchor=CENTER, width=85)
+    set.column("Weight", anchor=CENTER, width=85)
+    set.column("Height", anchor=CENTER, width=85)
 
-    tree = ttk.Treeview(columns=columns, show="headings")
-    tree.place(fill=BOTH, expand=1)
+    set.heading("#0", text="", anchor=CENTER)
+    set.heading("BMI", text="BMI", anchor=CENTER)
+    set.heading("Weight", text="Weight", anchor=CENTER)
+    set.heading("Height", text="Height", anchor=CENTER)
 
-    tree.heading("name", text="Имя")
-    tree.heading("age", text="Возраст")
-    tree.heading("email", text="Email")
+    global count
+    count = 0
 
-    for person in people:
-        tree.insert("", END, values=person)
+    with open("history.txt", "r") as file:
+        text = file.readline()
+        while text is not None:
+            value = text.split()
+            set.insert(parent='', index='end', iid=count, text='',
+                       values=(value[0], value[1], value[2]))
+            text = file.readline()
+            count += 1
 
-    scrollbar = ttk.Scrollbar(orient=VERTICAL, command=tree.yview)
-    tree.configure(yscroll=scrollbar.set)
-    scrollbar.grid(row=0, column=1, sticky="ns")
     history_w.mainloop()
 
 
@@ -95,18 +105,17 @@ weight_tf = Entry()
 weight_tf.place(x=270, y=110, width=130)
 
 cal_btn = Button(
- text='BMI',
- command=calculate_bmi,
- font=('Terminal',10)
+    text='BMI',
+    command=calculate_bmi,
+    font=('Terminal', 10)
 )
 cal_btn.place(x=185, y=160)
 
-
 btn_theme = Button(
-    bg = "black",
+    bg="black",
     fg="white",
     text="Dark theme",
-    command = change_color,
+    command=change_color,
     font=('Terminal', 10)
 )
 btn_theme.place(x=200, y=250)
